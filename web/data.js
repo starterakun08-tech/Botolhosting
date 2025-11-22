b405ad554e1b726e0303150d942de49ce783476b
starterakun08-tech <starterakun08@gmail.com>
2025-11-22 17:56:43 +0700

    Deploy web via WhatsApp bot - web/data.js

---
+++ web/data.js
@@ -0,0 +1,21 @@
+
+const places = [
+    {
+        id: 1,
+        name: "Aone Trawas",
+        category: "cafe", 
+        district: "Trawas",
+        desc: "Tempat ngopi nyaman dengan view pegunungan.",
+        img: "img/img1.jpg"
+    },
+    {
+        id: 2,
+        name: "Antasena Cafe",
+        category: "cafe",
+        district: "Trawas",
+        desc: "Area Caffe luas, cocok untuk nongkrong.",
+        img: "img/img2.jpg"
+    },
+    // ... (lanjutkan sendiri data sampai id 67)
+];
+export default places;
